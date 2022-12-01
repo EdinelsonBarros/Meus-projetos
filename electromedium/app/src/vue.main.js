ff4c7e5a8b4bc340cda3849c1344d796e3f6ff90
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2022-12-01 12:13:01 -0300

    Update

--- electromedium/app/src/vue.main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './VueApp.vue'
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-createApp(App).mount('#app')
--- vue/try/src/Button.js
+++
@@ -1,9 +0,0 @@
-const HelloVueApp = {
-  data() {
-    return {
-      message: 'Olá Vue!'
-    }
-  }
-}
-
-Vue.createApp(HelloVueApp).mount('#hello-vue')
